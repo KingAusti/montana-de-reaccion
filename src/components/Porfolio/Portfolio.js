be62a8d4fa84c92cff4e83b00b354a1481f9396c
KingAusti <austin@thehenrys.xyz>
2022-06-12 18:36:43 -0700

    adding assets

--- src/components/Porfolio/Portfolio.js
+++ src/components/Porfolio/Portfolio.js
@@ -1,3 +1,5 @@
+import './Portfolio';
+
 function Portfolio() {
     return (
       <div className="Portfolio">
@@ -6,9 +8,9 @@ function Portfolio() {
         <h1>This is the Portfolio</h1>
 
           {/* <!--Most influential project in largest box element--> */}
-            <a href='https://pokebuilder-v2.herokuapp.com/' id="pokebuilder" className="pokebuilder">test</a>
+            <a href='https://pokebuilder-v2.herokuapp.com/' id="pokebuilder" className="pokebuilder">pokebuilder v2</a>
             {/* <!--Previous project stacked veritically under the most important project--> */}
-            <a href='https://kingausti.github.io/homework_Jan22/' id="horiseon" className="horiseon">test</a>
+            <a href='https://kingausti.github.io/Weathers-R-Us/' id="horiseon" className="weather-dashboard">weather-dashboard</a>
             {/* <!--second project in noticabley smaller box element--> */}
             <div class = "lower-projects">
             <a href='https://kingausti.github.io/run-buddy/'  id="run-buddy" className="run-buddy">test</a>
